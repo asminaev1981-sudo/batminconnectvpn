353e2fc29f20998914a234c3a349361045cf77b7
root <root@batmin-platform.local>
2026-08-10 07:45:44 +0200

    Fix application startup widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,9 +4,6 @@ import 'package:batmin_connect/main.dart';
 void main() {
   testWidgets('application starts', (WidgetTester tester) async {
     await tester.pumpWidget(const BatminConnectApp());
-
-    await tester.pump(const Duration(seconds: 1));
-
     expect(find.byType(BatminConnectApp), findsOneWidget);
   });
 }
