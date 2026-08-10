91ca891d2eb7561e9ac5b7d46a07161b3d5326e9
root <root@batmin-platform.local>
2026-08-10 06:52:27 +0200

    Fix splash timer in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@ void main() {
   testWidgets('application starts', (WidgetTester tester) async {
     await tester.pumpWidget(const BatminConnectApp());
 
+    await tester.pump(const Duration(seconds: 1));
+
     expect(find.byType(BatminConnectApp), findsOneWidget);
   });
 }
